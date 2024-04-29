11d7c8d2f53a9165528147cd3fcb0be38efcbcae
hussam.osmani <hussam.osmani@accenture.com>
2024-04-29 19:10:31 +1000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Box } from '@mui/material';
+import OptionsPanel from 'components/global/OptionsPanel';
+import React from 'react';
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <OptionsPanel />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/global/OptionsPanel.tsx
@@ -0,0 +1,38 @@
+import { Drawer } from '@mui/material';
+import ListItemButton from '@mui/material/ListItemButton';
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MessageIcon from '@mui/icons-material/Message';
+import PersonIcon from '@mui/icons-material/Person';
+const OptionsPanel = () => {
+    const drawerWidth = 240;
+
+    return (
+        <Drawer variant="permanent" sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+                width: drawerWidth,
+                boxSizing: 'border-box',
+            },
+        }}>
+            <List>
+                {['Contacts', 'Messages'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <PersonIcon /> : <MessageIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Drawer>
+    );
+}
+
+export default OptionsPanel;
